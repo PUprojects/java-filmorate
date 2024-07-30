e528ae61ec7f8bd670e9da62c5d0a67e75198f38
Pavel Usoltsev <usoltsevdisk@yandex.ru>
2024-07-30 21:29:08 +0300

    Исправлены ошибки стиля (пустые строки.. некрасиво,но надо)

--- src/main/java/ru/yandex/practicum/filmorate/model/Marker.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Marker.java
@@ -1,6 +1,9 @@
 package ru.yandex.practicum.filmorate.model;
 
 public interface Marker {
-    interface OnCreate {}
-    interface OnUpdate {}
+    interface OnCreate {
+    }
+
+    interface OnUpdate {
+    }
 }
