14340a0a195a2818ebef55a8d1a8e80fbda40b53
Pavel Usoltsev <usoltsevdisk@yandex.ru>
2024-07-31 19:37:10 +0300

    Добавлена аннотация @FieldDefaults с заданием уровня доступа полей private для классов фильма и пользователя

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,26 +1,29 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
 public class Film {
     @NotNull(groups = Marker.OnUpdate.class,
             message = "Не задан id фильма")
-    private long id;
+    long id;
     @NotBlank(groups = {Marker.OnCreate.class, Marker.OnUpdate.class},
             message = "Название фильмане может быть пустым")
-    private String name;
+    String name;
     @Size(groups = {Marker.OnCreate.class, Marker.OnUpdate.class},
             max = 200,
             message = "Описание не должно быть длинее 200 символов")
-    private String description;
+    String description;
     @ReleaseDate(groups = {Marker.OnCreate.class, Marker.OnUpdate.class},
             message = "Дата выхода фильма должна быть не ранее 28.12.1985")
-    private LocalDate releaseDate;
+    LocalDate releaseDate;
     @Positive(groups = {Marker.OnCreate.class, Marker.OnUpdate.class},
             message = "Продолжительность должна быть целым числом")
-    private int duration;
+    int duration;
 }
--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,25 +1,28 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Data
 public class User {
     @NotNull(groups = Marker.OnUpdate.class,
             message = "Не задан id пользователя")
-    private long id;
+    long id;
     @NotBlank(groups = {Marker.OnCreate.class, Marker.OnUpdate.class},
             message = "Адрес почты не может быть пустым")
     @Email(groups = {Marker.OnCreate.class, Marker.OnUpdate.class},
             message = "Ошибка в формате email")
-    private String email;
+    String email;
     @NotBlank(groups = {Marker.OnCreate.class, Marker.OnUpdate.class},
             message = "Логин не должен быть пустым")
-    private String login;
-    private String name;
+    String login;
+    String name;
     @Past(groups = {Marker.OnCreate.class, Marker.OnUpdate.class},
             message = "Дата рождения должна быть в прошлом")
-    private LocalDate birthday;
+    LocalDate birthday;
 }
